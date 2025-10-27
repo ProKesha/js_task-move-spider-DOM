7f3fe235ad0b61f399d58ad12d59b4e650a8f833
Dmytro Popov <lort20178@gmail.com>
2025-10-27 20:20:10 +0200

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const BORDER = 10;
+const SPIDER_SIZE = 50;
+
+spider.style.position = 'absolute';
+spider.style.transition = 'top 0.3s ease, left 0.3s ease';
+
+wall.addEventListener('click', (e) => {
+  const wallRect = wall.getBoundingClientRect();
+
+  const clickX = e.clientX - wallRect.left - BORDER;
+  const clickY = e.clientY - wallRect.top - BORDER;
+
+  const maxLeft = wall.offsetWidth - BORDER * 2 - SPIDER_SIZE;
+
+  const maxTop = wall.offsetHeight - BORDER * 2 - SPIDER_SIZE;
+
+  let newLeft = clickX - SPIDER_SIZE / 2;
+  let newTop = clickY - SPIDER_SIZE / 2;
+
+  newLeft = Math.max(0, Math.min(newLeft, maxLeft));
+  newTop = Math.max(0, Math.min(newTop, maxTop));
+
+  spider.style.left = `${newLeft}px`;
+  spider.style.top = `${newTop}px`;
 });
